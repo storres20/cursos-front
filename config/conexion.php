266a4ef08d4045b4166e836595587d11b3a07b7d
storres20 <sanchotorres777@gmail.com>
2022-04-29 09:41:55 -0500

    ADD databse connection

---
+++ config/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+  // Clever-cloud - mysql
+  $host = 'bjip4tf1cstzcyhhev7r-mysql.services.clever-cloud.com';
+  $user = 'ugvlpqynnm7koc6u';
+  $pass = 'x6IVPuAiGLzujR6ukV6A';
+  $db = 'bjip4tf1cstzcyhhev7r';
+  
+  $conn = mysqli_connect($host, $user, $pass, $db);
+  
+  /* if ($conn) {
+    echo "conectado";
+  } */
+
+
+?>
