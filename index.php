ae79d63081a21e1c0e105720d25ac6e3cf2be458
storres20 <sanchotorres777@gmail.com>
2022-04-29 09:18:34 -0500

    ADD login form

--- index.php
+++ index.php
@@ -8,10 +8,26 @@
   <!-- CSS Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   
+  <link rel="stylesheet" href="style.css">
+  
   <title>Login</title>
 </head>
 <body>
-  
+  <div class="row mt-5">
+    <div class="col-1 col-sm-1 col-md-3 col-lg-4"></div>
+    <div class="col-10 col-sm-10 col-md-6 col-lg-4">
+      <form action="" method="post">
+        <h1><strong>Login</strong></h1>
+        <input type="text" name="" id="" class="form-control" placeholder="usuario" required="true" maxlength="10" minlength="3">
+        <br>
+        <input type="password" name="" id="" class="form-control" placeholder="password" required="true" maxlength="10" minlength="3">
+        <br>
+        <button type="submit" class="btn btn-primary">Ingresar</button>
+        
+      </form>
+    </div>
+    <div class="col-1 col-sm-1 col-md-3 col-lg-4"></div>
+  </div>
   
   
   
