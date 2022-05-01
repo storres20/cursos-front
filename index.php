e397e62c19685df956dc9d69cb680b1b4cb1acb9
storres20 <sanchotorres777@gmail.com>
2022-05-01 00:00:57 -0500

    ADD footer

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   <title>Login</title>
 </head>
 <body>
-  <div class="row mt-5">
+  <div class="login mt-5">
     <div class="col-1 col-sm-1 col-md-3 col-lg-4"></div>
     <div class="col-10 col-sm-10 col-md-6 col-lg-4">
       <form id="formulario" method="post" action="validalogin.php?op=in">
@@ -35,7 +35,7 @@
   </div>
   
   
-  
+  <?php include 'template/footer.php' ?>
   
   <!-- JS Bootstrap 5 -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
