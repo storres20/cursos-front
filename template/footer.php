6f5505d11bb9d70ccfc7bdaf469ac7a2cc299a1f
storres20 <sanchotorres777@gmail.com>
2022-05-01 00:00:42 -0500

    UPDATE

---
+++ template/footer.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid bg-dark fixed-bottom">
+  <div class="row">
+    <div class="col-md text-light text-center py-3">
+        Desarrollador por @@@
+    </div>
+  </div>
+</footer>
