7d5c334727b7c128daf1d995caec26047073e46e
storres20 <sanchotorres777@gmail.com>
2022-04-30 21:19:25 -0500

    UPDATE to multi login

--- validalogin.php
+++ validalogin.php
@@ -1,18 +1,44 @@
 <?php
+  session_start();
+  $usuario = $_POST['usuario'] ? $_POST['usuario'] : "";
+  $password = $_POST['password'] ? $_POST['password'] : "";
+  $op = $_GET['op'];
+  
   include("config/conexion.php");
   
-  if (isset($_POST['usuario']) && !empty($_POST['usuario']) && isset($_POST['password']) && !empty($_POST['password'])) {
-    $usuario = $_POST['usuario'];
-    $password = $_POST['password'];
+  if($op=="in"){
     
-    $query = "SELECT * FROM usuarios WHERE (nombre = '$usuario' && password = '$password')";
+    $query = "SELECT * FROM usuarios WHERE (usuario = '$usuario' && password = '$password')";
     $result = mysqli_query($conn, $query);
     
     if (mysqli_num_rows($result) > 0) {
-      echo json_encode(array('success' => 1));
+      
+      $qry = mysqli_fetch_array($result);
+      $_SESSION['usuario'] = $qry['usuario'];
+      $_SESSION['password'] = $qry['password'];
+      $_SESSION['rol'] = $qry['rol'];
+      
+      if($qry['rol']=="admin"){
+          header("location:homeAdmin.php");
+      }else if($qry['rol']=="alumno"){
+          header("location:home.php");
+      }
+      
     }else{
-      echo json_encode(array('success' => 0));
+      
+      ?>
+        <script language="JavaScript">
+            alert('El nombre de usuario o la contraseña no coinciden. ¡Por favor repita de nuevo!');
+            document.location='index.php';
+        </script>
+      <?php
+      
     }
-  }
+  }else if($op=="out"){
+    unset($_SESSION['usuario']);
+    unset($_SESSION['password']);
+    unset($_SESSION['rol']);
+    header("location:index.php");
+}
 
 ?>
