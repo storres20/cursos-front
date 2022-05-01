ee968d5d6cf2315543223b37aac3248339bf7356
storres20 <sanchotorres777@gmail.com>
2022-05-01 01:25:14 -0500

    RENDER data

--- paginas/home/home.php
+++ paginas/home/home.php
@@ -5,6 +5,17 @@
   }
   
   $usuario = $_SESSION['usuario'];
+  
+  include_once '../../config/conexion.php';
+  $query = "SELECT * FROM preguntas";
+  $result = mysqli_query($conn, $query);
+  //$rows = array();
+  
+ /*  $preguntas = mysqli_fetch_array($result);
+  print_r($preguntas); */
+  
+  //$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+  //print_r($row)
 ?>
 
 <?php include '../../template/header.php' ?>
@@ -17,12 +28,46 @@
   
   <h1>Cuestionario de preguntas</h1>
   
+  
+  
+  <?php
+    $std_num = 1;
+    while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+  ?>
+  
   <div class="card-preg">
-    <h2>Pregunta #00</h2>
-    <input type="text" name="" id="">
+    <h2>Pregunta #<?php echo $std_num; ?></h2>
+    
+    <input type="text" name="" id="" value="<?php echo $row['pregunta']; ?>" disabled style="width:100%">
     <br><br>
-    <textarea name="" id="" cols="30" rows="10"></textarea>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        <?php echo $row['alt1']; ?>
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        <?php echo $row['alt2']; ?>
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        <?php echo $row['alt3']; ?>
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        <?php echo $row['alt4']; ?>
+      </label>
+    </div>
+    
   </div>
+    <?php $std_num++;} ?>
+  
   
   
   
