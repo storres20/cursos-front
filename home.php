96f8d90e709f711e8f0209a00a89249ada17409c
storres20 <sanchotorres777@gmail.com>
2022-04-30 21:22:54 -0500

    ADD bootstrap5

--- home.php
+++ home.php
@@ -11,14 +11,22 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- CSS Bootstrap 5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
   <title>Document</title>
 </head>
 <body>
   <h1>Bienvenido Alumno</h1>
   
   <form action="validalogin.php?op=out" method="POST">
-    <button type="submit">Cerrar Session</button>
+    <button type="submit" class="btn btn-primary">Cerrar Session</button>
   </form>
   
+  
+  
+  <!-- JS Bootstrap 5 -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
