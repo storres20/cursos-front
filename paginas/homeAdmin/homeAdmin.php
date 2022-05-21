565b5ac37db02d97a22483578edeffe55d85fc1f
storres20 <sanchotorres777@gmail.com>
2022-05-21 16:04:20 -0500

    UPDATE

--- paginas/homeAdmin/homeAdmin.php
+++ paginas/homeAdmin/homeAdmin.php
@@ -4,6 +4,8 @@
     header("location:../../index.php?mensaje=usuario");
   }
   
+  $usuario = $_SESSION['usuario'];
+  
   include_once '../../config/conexion.php';
   $query = "SELECT * FROM respuestas";
   $result = mysqli_query($conn, $query);
@@ -11,32 +13,86 @@
 ?>
 
 <?php include '../../template/header.php' ?>
-
-  <h1>Bienvenido Administrador</h1>
   
-  <form action="../../validalogin.php?op=out" method="POST">
-    <button type="submit" class="btn btn-primary">Cerrar Session</button>
-  </form>
+  <div class="card" style="margin: 20px;">
+    <div class="card-body">
+      <h1 class="card-title">Bienvenido Admin <?php echo $usuario?></h1>
+      
+      <form action="../../validalogin.php?op=out" method="POST">
+        <button type="submit" class="btn btn-primary">Cerrar Session</button>
+      </form>
+      
+      <br><br>
   
-  <br><br>
+      <h3 class="text-muted"><?php echo ($rows > 0) ? "El Examen Random ya ha sido iniciado" : "Iniciar el Examen Random"; ?></h3>
+      <form action="random.php" method="POST">
+        <button type="submit" class="btn btn-primary" <?php echo ($rows > 0) ? "disabled" : ""; ?> >Random - Curso X</button>
+      </form>
+      
+      <br>
   
-  <h3><?php echo ($rows > 0) ? "El Examen Random ya ha sido iniciado" : "Iniciar el Examen Random"; ?></h3>
-  <form action="random.php" method="POST">
-    <button type="submit" class="btn btn-primary" <?php echo ($rows > 0) ? "disabled" : ""; ?> >Random - Curso X</button>
-  </form>
-  
-  <br>
-  
-  <?php
-    if (isset($_GET['mensaje']) && $_GET['mensaje'] == 'ok') {
-  ?>
-  <div class="alert alert-success alert-dismissible fade show" role="alert">
-    <strong>Examen Random asignado con Exito!</strong>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      <?php
+        if (isset($_GET['mensaje']) && $_GET['mensaje'] == 'ok') {
+      ?>
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Examen Random asignado con Exito!</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <?php } ?>
+      
+    </div>
   </div>
-  <?php } ?>
+  
+  
+  <!-- Listado de Alumnos -->
+  <div class="card" style="margin: 20px;">
+    <div class="card-body">
+      <h5 class="card-title">Listado de Alumnos</h5>
+      
+      <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+          <tr>
+              <th>N°</th>
+              <th>Alumno</th>
+              <th>Estado Exámen</th>
+              <th>Acción</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>juan</td>
+            <td>Finalizado</td>
+            <td><button class="btn btn-danger" title="Restaurar Exámen"><i class="bi bi-arrow-clockwise"></i></button> </td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>N°</th>
+            <th>Alumno</th>
+            <th>Estado Exámen</th>
+            <th>Acción</th>
+          </tr>
+        </tfoot>
+      </table>
+      
+    </div>
+  </div>
   
   <!-- JS Bootstrap 5 -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
+  <!-- Jquery -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  
+  <!-- DataTable -->
+  <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
+  
+  <script>
+    $(document).ready(function () {
+        $('#example').DataTable();
+    });
+  </script>
+  
 </body>
 </html>
