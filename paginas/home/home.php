cf29fad70d1eb5109ecd7683e5a0cc96534d271b
storres20 <sanchotorres777@gmail.com>
2022-05-21 07:00:47 -0500

    ADD card style

--- paginas/home/home.php
+++ paginas/home/home.php
@@ -1,5 +1,8 @@
 <?php
+  /* Carga los datos de Inicio Sesion */
   session_start();
+  
+  /* Verifica que no haya ursurpacion de Roles */
   if ($_SESSION['rol'] !== "alumno" || $_SESSION['rol'] === "admin") {
     header("location:../../index.php?mensaje=usuario");
   }
@@ -7,6 +10,7 @@
   $usuario = $_SESSION['usuario'];
   $id_usuario = $_SESSION['id'];
   
+  /* Conexion a Base de Datos */
   include_once '../../config/conexion.php';
   //$query = "SELECT * FROM preguntas WHERE click = 0";
   $query = "SELECT * FROM respuestas WHERE id_usuario=$id_usuario";
@@ -14,50 +18,58 @@
 ?>
 
 <?php include '../../template/header.php' ?>
-
-  <h1>Bienvenido <?php echo $usuario?></h1>
   
-  <form action="../../validalogin.php?op=out" method="POST">
-    <button type="submit" class="btn btn-primary">Cerrar Session</button>
-  </form>
-  
-  <h1>Cuestionario de preguntas</h1>
-  
-  <!-- Mensaje de Alerta de prevencion de fraude -->
-  <?php
-    if (isset($_GET['mensaje']) && $_GET['mensaje'] == 'fraude') {
-  ?>
-  <div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <strong>Tenga cuidado!</strong> Resuelva solo su examen.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  <div class="card" style="margin: 20px;">
+    <div class="card-body">
+      <h1 class="card-title">Bienvenido <?php echo $usuario?></h1>
+      <form class="mb-2" action="../../validalogin.php?op=out" method="POST">
+        <button type="submit" class="btn btn-primary">Cerrar Session</button>
+      </form>
+      <h2 class="card-subtitle mb-2 text-muted">Cuestionario de preguntas</h2>
+      
+      <!-- Mensaje de Alerta de prevencion de fraude -->
+      <?php
+        if (isset($_GET['mensaje']) && $_GET['mensaje'] == 'fraude') {
+      ?>
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Tenga cuidado!</strong> Resuelva solo su examen.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <?php } ?>
+      
+      <!-- Table - Cuestionario de Preguntas -->
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th class="text-center" scope="col">N°</th>
+            <th scope="col">Pregunta</th>
+            <th scope="col">Accion</th>
+          </tr>
+        </thead>
+        <tbody>
+        
+          <?php
+            $std_num = 1;
+            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+          ?>
+        
+          <tr>
+            <th class="text-center" scope="row"><?php echo $std_num; ?></th>
+            <td><?php echo $row['pregunta']; ?></td>
+            <td>
+              <a id="<?php echo $row['id'];?>" class="btn <?php echo ($row['click']==='0') ? 'btn-primary' : 'btn-secondary'; ?>" href="responder.php?id=<?php echo $row['id'];?>"><?php echo ($row['click']==="0") ? "Entrar" : "Enviado"; ?></a>
+            </td>
+          </tr>
+        
+          <?php $std_num++;} ?>
+      
+        </tbody>
+      </table>
+      
+      <!-- Button - Finalizar Examen -->
+      <button type="button" class="btn btn-danger">Finalizar Examen</button>
+    </div>
   </div>
-  <?php } ?>
-  
-  <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">N°</th>
-      <th scope="col">Pregunta</th>
-      <th scope="col">Accion</th>
-    </tr>
-  </thead>
-  <tbody>
-  
-  <?php
-    $std_num = 1;
-    while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-  ?>
-  
-    <tr>
-      <th scope="row"><?php echo $std_num; ?></th>
-      <td><?php echo $row['pregunta']; ?></td>
-      <td><a id="<?php echo $row['id'];?>" class="btn <?php echo ($row['click']==='0') ? 'btn-primary' : 'btn-secondary'; ?>" href="responder.php?id=<?php echo $row['id'];?>"><?php echo ($row['click']==="0") ? "Entrar" : "Enviado"; ?></a></td>
-    </tr>
-  
-  <?php $std_num++;} ?>
-  
-    </tbody>
-</table>
   
   
   <!-- JS Bootstrap 5 -->
