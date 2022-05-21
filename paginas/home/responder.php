c747c1b8986d265bcb6afda6924a0ce574853759
storres20 <sanchotorres777@gmail.com>
2022-05-21 15:18:41 -0500

    UPDATE

--- paginas/home/responder.php
+++ paginas/home/responder.php
@@ -27,6 +27,8 @@
       <?php
         $std_num = 1;
         while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+        
+          $_SESSION['cant'] = $row['cant'];
           
           //para prevencion en caso un alumno ingrese URL forzado de pregunta de otro compañero de clase
           if ($row['id_usuario'] !== $id_usuario) {
@@ -39,6 +41,7 @@
         
         <!-- <input type="text" name="" id="" value="<?php echo $row['pregunta']; ?>" disabled style="width:100%"> -->
         <h4><?php echo $row['pregunta']; ?></h4>
+        <h6><b>Nota:</b> Marcar "<?php echo $row['cant']; ?>" alternativa(s)</h6>
         
         <form action="enviar.php?id=<?php echo $id_pregunta; ?>" method="post">
           <div class="form-check">
@@ -81,8 +84,26 @@
     </div>
   </div>
   
+  <?php
+    $cant = $_SESSION['cant'];
+  ?>
   
   <!-- JS Bootstrap 5 -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
+  <script type="text/javascript">
+    var checks = document.querySelectorAll(".form-check-input");
+    /* var max = 2; */
+    var max = <?php echo $cant; ?>;
+    
+    for (var i = 0; i < checks.length; i++)
+      checks[i].onclick = selectiveCheck;
+    function selectiveCheck (event) {
+      var checkedChecks = document.querySelectorAll(".form-check-input:checked");
+      if (checkedChecks.length >= max + 1)
+        return false;
+    }
+  </script>
+  
 </body>
 </html>
