570ed6d4aa083cea12058b6b0c029073d322a607
storres20 <sanchotorres777@gmail.com>
2022-05-21 17:03:22 -0500

    ADD commits

--- paginas/homeAdmin/restaurar.php
+++ paginas/homeAdmin/restaurar.php
@@ -8,12 +8,16 @@ session_start();
   /* Captura el ID que viene en la URL */
   $id_usuario = $_GET['id'];
   
+  /* Conexion a Mysql */
   include_once '../../config/conexion.php';
   
+  /* Se restaura la opcion de volver a rendir el examen si el campo FINALIZADO está en CERO */
   $finalizado = '0';
   
+  /* Comando SQL para actualizar el campo FINALIZADO a CERO */
   $query = "UPDATE respuestas SET finalizado='$finalizado' WHERE id_usuario='$id_usuario'";
   
+  /* Una vez ejecutado el SQL se verifica y redirige al menu inicial */
   if ($conn->query($query) === TRUE) {
     //echo "New record created successfully";
     header("location:homeAdmin.php");
